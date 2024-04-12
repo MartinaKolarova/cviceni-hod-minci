3ace5a5d83541c241f5e53a047109464f89ea7de
Martina Kolářová <ruzickova1.martina@gmail.com>
2024-04-12 19:23:27 +0200

    Exercise done

---
+++ index.js
@@ -0,0 +1,12 @@
+const vysledek = document.querySelector('.vysledek');
+const mince = document.querySelector('.mince');
+
+const padlOrel = Math.random() < 0.5;
+
+if (padlOrel) {
+  vysledek.textContent = 'Padl orel';
+  mince.classList.add('mince--orel');
+} else {
+  vysledek.textContent = 'Padla panna';
+  mince.classList.add('mince--panna');
+}
